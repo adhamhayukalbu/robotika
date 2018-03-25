0f950603a380a34446e8efdf1c5f76b031890caa
Adham Hayukalbu <sojoy.enjoyaja@gmail.com>
2018-03-25 21:03:44 +0700

    [IMP] Rename files & Update wrong index

--- 2_traffic_light.ino
+++ 2_traffic_light.ino
@@ -1,11 +1,12 @@
 int timer = 5000;
 int led_1_pins[] = {
-  1, 2, 3
+  2, 3, 4
 };
 int led_2_pins[] = {
-  4, 5, 6
+  5, 6, 7
 };
 int led_counts = 3;
+int ground_pin = 9;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -14,6 +15,7 @@ void setup() {
        pinMode(led_1_pins[p], OUTPUT); // Set the mode to OUTPUT
        pinMode(led_2_pins[p], OUTPUT);
    }
+   pinMode(groud_pin, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
@@ -24,22 +26,21 @@ void loop() {
    digitalWrite(led_1_pins[1], 1);
    digitalWrite(led_2_pins[1], 1);    
    delay(timer-4000);
-   for(int p=0; p<2; p++){
-      digitalWrite (led_1_pins[p], 0); // merah & kuning mati
-   }
-   for(int p=1; p<2; p++){
-      digitalWrite (led_2_pins[p], 0); // hijau & kuning mati
-   }
+
+   digitalWrite(led_1_pins[0], 0);
+   digitalWrite(led_1_pins[1], 0);
+   digitalWrite(led_2_pins[2], 0);
+   digitalWrite(led_2_pins[1], 0);
+
    digitalWrite (led_1_pins[2], 1); // hijau simpangan 1 nyala 5 detik
-   digitalWrite(led_1_pins[0], 1); // merah simpangan 2 nyala 5 detik
+   digitalWrite(led_2_pins[0], 1); // merah simpangan 2 nyala 5 detik
    delay(timer-1000);
    digitalWrite (led_1_pins[1], 1);
    digitalWrite(led_2_pins[1], 1);
    delay(timer-4000);
-   for(int p=0; p<2; p++){
-      digitalWrite (led_1_pins[p], 0); // merah & kuning mati
-   }
-   for(int p=1; p<2; p++){
-      digitalWrite (led_2_pins[p], 0); // hijau & kuning mati
-   }
+
+   digitalWrite(led_2_pins[2], 0);
+   digitalWrite(led_2_pins[1], 0);
+   digitalWrite(led_1_pins[0], 0);
+   digitalWrite(led_1_pins[1], 0);
 }
