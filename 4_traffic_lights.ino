6f1517911876dd8160ce7f1ee60104ee383fe10d
Adham Hayukalbu <sojoy.enjoyaja@gmail.com>
2018-03-26 15:21:40 +0700

    [IMP] - Update Condition

--- 4_traffic_lights.ino
+++ 4_traffic_lights.ino
@@ -26,59 +26,59 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-   digitalWrite(led_1_pins[0], 1); // merah simpangan 1 nyala 5 detik
+   digitalWrite(led_1_pins[2], 1); // hijau simpangan 1 nyala 5 detik
    digitalWrite(led_2_pins[0], 1); // merah simpangan 2 nyala 5 detik
    digitalWrite(led_3_pins[0], 1); // merah simpangan 3 nyala 5 detik
-   digitalWrite(led_4_pins[2], 1); // hijau simpangan 4 nyala 5 detik  
+   digitalWrite(led_4_pins[0], 1); // merah simpangan 4 nyala 5 detik  
    delay(timer-1000); // 4 detik
-   digitalWrite(led_3_pins[1], 1); // kuning simpangan 3 nyala 1 detik
-   digitalWrite(led_4_pins[1], 1); // kuning simpangan 4 nyala 1 detik
-   delay(timer-4000); // 1 detik
-
-   digitalWrite(led_3_pins[0], 0); // merah simpangan 3 mati
-   digitalWrite(led_3_pins[1], 0); // kuning simpangan 3 mati
-   digitalWrite(led_4_pins[2], 0); // hijau simpangan 4 mati
-   digitalWrite(led_4_pins[1], 0); // kuning simpangan 4 mati
-
-   digitalWrite(led_1_pins[0], 1); // merah simpangan 1 nyala 5 detik
-   digitalWrite(led_2_pins[0], 1); // merah simpangan 2 nyala 5 detik
-   digitalWrite(led_3_pins[2], 1); // hijau simpangan 3 nyala 5 detik
-   digitalWrite(led_4_pins[0], 1); // merah simpangan 4 nyala 5 detik
-   delay(timer-1000); // 4 detik
+   digitalWrite(led_1_pins[1], 1); // kuning simpangan 1 nyala 1 detik
    digitalWrite(led_2_pins[1], 1); // kuning simpangan 2 nyala 1 detik
-   digitalWrite(led_3_pins[1], 1); // kuning simpangan 3  nyala 1 detik
    delay(timer-4000); // 1 detik
 
-   digitalWrite(led_3_pins[2], 0); // hijau simpangan 3 mati
-   digitalWrite(led_3_pins[1], 0); // kuning simpangan 3 mati
-   digitalWrite(led_2_pins[0], 0); // merah simpangan 2 mati
-   digitalWrite(led_2_pins[1], 0); // kuning simpangan 2 mati
+   digitalWrite(led_1_pins[2], 0); // hijau simpangan 3 mati
+   digitalWrite(led_1_pins[1], 0); // kuning simpangan 3 mati
+   digitalWrite(led_2_pins[0], 0); // merah simpangan 4 mati
+   digitalWrite(led_2_pins[1], 0); // kuning simpangan 4 mati
 
    digitalWrite(led_1_pins[0], 1); // merah simpangan 1 nyala 5 detik
    digitalWrite(led_2_pins[2], 1); // hijau simpangan 2 nyala 5 detik
    digitalWrite(led_3_pins[0], 1); // merah simpangan 3 nyala 5 detik
    digitalWrite(led_4_pins[0], 1); // merah simpangan 4 nyala 5 detik
    delay(timer-1000); // 4 detik
-   digitalWrite (led_2_pins[1], 1); // kuning simpangan 2 nyala 1 detik
-   digitalWrite(led_1_pins[1], 1); // kuning simpangan 1  nyala 1 detik
+   digitalWrite(led_2_pins[1], 1); // kuning simpangan 2 nyala 1 detik
+   digitalWrite(led_3_pins[1], 1); // kuning simpangan 3  nyala 1 detik
    delay(timer-4000); // 1 detik
 
-   digitalWrite(led_2_pins[2], 0); // hijau simpangan 2 mati
-   digitalWrite(led_2_pins[1], 0); // kuning simpangan 2 mati
-   digitalWrite(led_1_pins[0], 0); // merah simpangan 1 mati
-   digitalWrite(led_1_pins[1], 0); // kuning simpangan 1 mati
+   digitalWrite(led_2_pins[2], 0); // hijau simpangan 3 mati
+   digitalWrite(led_2_pins[1], 0); // kuning simpangan 3 mati
+   digitalWrite(led_3_pins[0], 0); // merah simpangan 2 mati
+   digitalWrite(led_3_pins[1], 0); // kuning simpangan 2 mati
 
-   digitalWrite(led_1_pins[2], 1); // hijau simpangan 1 nyala 5 detik
+   digitalWrite(led_1_pins[0], 1); // merah simpangan 1 nyala 5 detik
+   digitalWrite(led_2_pins[0], 1); // merah simpangan 2 nyala 5 detik
+   digitalWrite(led_3_pins[2], 1); // hijau simpangan 3 nyala 5 detik
+   digitalWrite(led_4_pins[0], 1); // merah simpangan 4 nyala 5 detik
+   delay(timer-1000); // 4 detik
+   digitalWrite (led_3_pins[1], 1); // kuning simpangan 3 nyala 1 detik
+   digitalWrite(led_4_pins[1], 1); // kuning simpangan 4  nyala 1 detik
+   delay(timer-4000); // 1 detik
+
+   digitalWrite(led_3_pins[2], 0); // hijau simpangan 3 mati
+   digitalWrite(led_3_pins[1], 0); // kuning simpangan 3 mati
+   digitalWrite(led_4_pins[0], 0); // merah simpangan 4 mati
+   digitalWrite(led_4_pins[1], 0); // kuning simpangan 4 mati
+
+   digitalWrite(led_1_pins[0], 1); // merah simpangan 1 nyala 5 detik
    digitalWrite(led_2_pins[0], 1); // merah simpangan 2 nyala 5 detik
    digitalWrite(led_3_pins[0], 1); // merah simpangan 3 nyala 5 detik
-   digitalWrite(led_4_pins[0], 1); // merah simpangan 4 nyala 5 detik
+   digitalWrite(led_4_pins[2], 1); // hijau simpangan 4 nyala 5 detik
    delay(timer-1000); // 4 detik
    digitalWrite (led_1_pins[1], 1); // kuning simpangan 1 nyala 1 detik
    digitalWrite(led_4_pins[1], 1); // kuning simpangan 4  nyala 1 detik
    delay(timer-4000); // 1 detik
 
-   digitalWrite(led_1_pins[2], 0); // hijau simpangan 1 mati
+   digitalWrite(led_1_pins[0], 0); // merah simpangan 1 mati
    digitalWrite(led_1_pins[1], 0); // kuning simpangan 1 mati
-   digitalWrite(led_4_pins[0], 0); // merah simpangan 4 mati
+   digitalWrite(led_4_pins[2], 0); // hijau simpangan 4 mati
    digitalWrite(led_4_pins[1], 0); // kuning simpangan 4 mati
 }
